6633344a3f0b40675d13220f269b9f3ede7f32f6
François <francois.deleu@telecom-paris.fr>
2024-07-06 15:51:34 +0200

    fixed mem issue with setup_string

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 char * setup_string(int, char**);
 
@@ -7,18 +8,24 @@ int main(int argc, char** argv) {
 
     char * expr = setup_string(argc, argv);
     printf("%s\n", expr);
+    free(expr);
 
     return 0;
 }
 
+/**Parses the given string array, turning it into a single string without
+ * any spaces. The returned string is allocated and must be freed;
+ */
 char * setup_string(int argc, char** argv) {
 
-    int expr_size = 0;
+    // find out concatenated size (+1 for '\0)
+    int expr_size = 1;
     for (int i = 1; i<argc; i++) {
         char * w = argv[i];
         while (*w != '\0') if (*(w++) != ' ') expr_size++;
     }
 
+    // error checks
     char * expr;
     if (!expr_size) { printf("no given expression\n"); return NULL; }
     if (!(expr = (char *) malloc(sizeof(char) * expr_size)))
@@ -35,6 +42,5 @@ char * setup_string(int argc, char** argv) {
         }
     }
 
-    printf("%s\n", expr);
     return expr;
 }
